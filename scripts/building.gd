30e49fcf57f0f73f370814b5706bb46ab3f7c7df
blueloveTH <blueloveth@dreamers.guru>
2022-08-05 19:47:13 +0800

    up

--- scripts/building.gd
+++ scripts/building.gd
@@ -1,23 +1,43 @@
 extends Node2D
-
 class_name Building
+
+signal completed()
+signal progress_changed(value)
 
 var progress: Vector2	# 进度/最大进度
 var level: int = 0		# 默认0级，建造完为1级
 
-var turn_based: TurnBasedDirector
-
-func is_active() -> bool:
-	"""已建造/升级完成"""
+func is_completed() -> bool:
 	return progress.x >= progress.y
 
 func _ready():
-	turn_based = get_tree().root.get_node("0/globals/turn_based")
-	turn_based.connect("round_started", self, "on_round_start")
-	turn_based.connect("round_ended", self, "on_round_end")
+	var turn_based = get_tree().root.get_node("0/globals/turn_based")
+	turn_based.connect("round_started", self, "_on_round_start_internal")
+	turn_based.connect("round_ended", self, "_on_round_end_internal")
 	
-func on_round_start(i):
+func add_progress(inc: int):
+	assert(not is_completed())
+	progress.x = min(progress.x+inc, progress.y)
+	emit_signal("progress_changed", progress)
+	if is_completed():
+		level += 1
+		progress.x = 0
+		emit_signal("completed")
+	
+func _on_round_start_internal(i):
+	if not is_completed():
+		add_progress(1)
+	else:
+		_on_round_start(i)
+
+func _on_round_end_internal(i):
+	if not is_completed():
+		add_progress(1)
+	else:
+		_on_round_end(i)
+
+func _on_round_start(i):
 	pass
 	
-func on_round_end(i):
+func _on_round_end(i):
 	pass
