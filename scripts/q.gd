be686c9edd7511e4e4dc3f32053e6558cc211968
blueloveTH <blueloveth@dreamers.guru>
2022-08-09 03:02:59 +0800

    up

--- scripts/q.gd
+++ scripts/q.gd
@@ -9,16 +9,8 @@ func get_camera_2d() -> Camera2D:
 func get_card_shop():
 	return get_node("/root/0/canvas/card_shop")
 
-
-
 func is_tap_event(event: InputEvent, pressed: bool=true):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.pressed == pressed:
-			return true
-	return false
-
-func is_double_tap_event(event: InputEvent):
-	if event is InputEventMouseButton:
-		if event.button_index == 1 and event.doubleclick:
 			return true
 	return false
