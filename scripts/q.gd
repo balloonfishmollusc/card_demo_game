37eb3f71936392d07d05cef3e7721594af020d98
blueloveTH <blueloveth@dreamers.guru>
2022-08-09 02:33:19 +0800

    up

--- scripts/q.gd
+++ scripts/q.gd
@@ -2,12 +2,23 @@ extends Node
 
 func get_p1():
 	return get_node("/root/0/globals/p1")
+
+func get_camera_2d() -> Camera2D:
+	return get_node("/root/0/world/Camera2D") as Camera2D
 	
 func get_card_shop():
 	return get_node("/root/0/canvas/card_shop")
-	
-func is_tap_event(event: InputEvent):
+
+
+
+func is_tap_event(event: InputEvent, pressed: bool=true):
 	if event is InputEventMouseButton:
-		if event.button_index == 1 and event.pressed:
+		if event.button_index == 1 and event.pressed == pressed:
+			return true
+	return false
+
+func is_double_tap_event(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and event.doubleclick:
 			return true
 	return false
